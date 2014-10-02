5888031c2b5068c4776f8a0b6264e000abc63914
Martins Kruze <martins.kruze@gmail.com>
2014-10-02 14:45:47 +0300

    pirmas lekcijas apraksta sakums

--- 01_lekcija_30_09_2014/kods/perimetrs.rb
+++ 01_lekcija_30_09_2014/kods/perimetrs.rb
@@ -6,3 +6,15 @@ otra_mala = 6
 puse_no_perimetra = viena_mala+otra_mala
 perimetrs = 2*puse_no_perimetra
 puts "perimetrs ir "+perimetrs.to_s
+
+
+a = 2 + 2  # a vērtība ir 4 (2+2)
+b = a + 2  # b vērtība ir 6 (4+2)
+a = a + 2  # a vērtība ir 6 (4+2)
+a = a * 2  # a vērtība ir 12 (6*2)
+c = a + b  # b vērtība ir 18 (12+6)
+
+a = 3      # a vērtība ir 3
+b = 2      # b vērtība ir 2
+c = a ** b # c vērtība ir 9 (3 kāpināts 2. pakāpē - 3*3)
+
