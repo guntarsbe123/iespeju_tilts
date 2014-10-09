8e59a85266e1b27ac3bfaf8f3fcf6807dd50d9b9
Martins Kruze <martins.kruze@gmail.com>
2014-10-09 23:16:09 +0300

    majasdarbi

---
+++ majas_01/skolas_zurnals.rb
@@ -0,0 +1,7 @@
+# 1. mājasdarba 2. uzdevums.
+
+skolas_zurnals = [{:vards => "Jānis", :uzvards => "Bērziņš", :atzime => 7},
+				  {:vards => "Pēteris", :uzvards => "Lauciņš", :atzime => 5},
+				  {:vards => "Edgars", :uzvards => "Kalniņš", :atzime => 10},
+				  {:vards => "Ziedonis", :uzvards => "Rasma", :atzime => 9},
+				  {:vards => "Jana", :uzvards => "Jansone", :atzime => 2}]
